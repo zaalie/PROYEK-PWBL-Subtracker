00f9c763a68904b15c8ee5a4d4391f969e3abb6f
zaalie <zaaalie24@gmail.com>
2025-12-18 20:43:14 +0700

    rei: menambahkan file auth.ts pada lib

--- backend/src/app/api/auth/route.ts
+++ backend/src/app/api/auth/route.ts
@@ -1,7 +1,19 @@
-import { ok } from "@/lib/response"
+import { prisma } from "@/lib/prisma";
+import { success, error } from "@/lib/response";
+import { comparePassword } from "@/lib/auth";
 
-export async function GET() {
-  return ok({
-    message: "Auth API ready",
-  })
+export async function POST(req: Request) {
+  const { email, password } = await req.json();
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) return error("User not found", 404);
+
+  const isValid = await comparePassword(password, user.password);
+  if (!isValid) return error("Wrong password", 401);
+
+  return success({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  });
 }
