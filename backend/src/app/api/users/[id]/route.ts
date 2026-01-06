2f6da87651b608e272ec12ed3177dddb8206ecba
zaalie <zaaalie24@gmail.com>
2026-01-06 23:32:38 +0700

    reiza : mengedit comment code program tampilkan data user di folder users pada file route.ts[id]

--- backend/src/app/api/users/[id]/route.ts
+++ backend/src/app/api/users/[id]/route.ts
@@ -1,23 +1,4 @@
 import { prisma } from "@/lib/prisma";
-import { apiResponse } from "@/lib/response";
+import { NextResponse } from "next/server";
 
-
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const id = Number(params.id); 
-
-  if (isNaN(id)) {
-    return apiResponse.badRequest("ID tidak valid");
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { id },
-    select: { id: true, name: true, email: true },
-  });
-
-  if (!user) return apiResponse.notFound();
-
-  return apiResponse.ok(user);
-}
+// Mengambil Satu Data User Berdasarkan ID //
