305a335c79761e11299aa35517d7cdbd7e5b275b
zaalie <zaaalie24@gmail.com>
2026-01-06 23:57:18 +0700

    reiza : membuat ulang code program pada route.ts di subscriptions[id]

--- backend/src/app/api/subscriptions/[id]/route.ts
+++ backend/src/app/api/subscriptions/[id]/route.ts
@@ -4,3 +4,18 @@ import { apiResponse } from "@/lib/response"
 type Params = { params: { id: string } }
 
 // GET SUBSCRIPTION BY ID //
+export async function GET(_: Request, { params }: Params) {
+  try {
+    const subscription = await prisma.subscription.findUnique({
+      where: { id: Number(params.id) },
+    })
+
+    if (!subscription) {
+      return apiResponse.notFound("Subscription tidak ditemukan")
+    }
+
+    return apiResponse.ok(subscription)
+  } catch (error) {
+    return apiResponse.serverError()
+  }
+}
