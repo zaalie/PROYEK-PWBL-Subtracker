cc132e1c8ec3080e2eae329ca9aadafe9c02a3ed
zaalie <zaaalie24@gmail.com>
2026-01-06 16:56:06 +0700

    reiza : tambah code di file route.ts pada subscriptions[id]

--- backend/src/app/api/subscriptions/[id]/route.ts
+++ backend/src/app/api/subscriptions/[id]/route.ts
@@ -1,3 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { apiResponse } from "@/lib/response";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = Number(params.id);
+
+  if (isNaN(id)) {
+    return apiResponse.badRequest("ID tidak valid");
+  }
+
+  const subscription = await prisma.subscription.findUnique({
+    where: { id },
+  }); 
