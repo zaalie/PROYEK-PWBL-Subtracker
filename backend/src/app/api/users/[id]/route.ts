9f0f10c9c1fcd26231fba9f17b576454cd393ff3
zaalie <zaaalie24@gmail.com>
2026-01-06 23:35:35 +0700

    reiza : menambahkan code program tampilkan data user di folder users pada file route.ts[id]

--- backend/src/app/api/users/[id]/route.ts
+++ backend/src/app/api/users/[id]/route.ts
@@ -2,3 +2,16 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 // Mengambil Salah Satu Data User Menurut Id //
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: Number(params.id) },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
