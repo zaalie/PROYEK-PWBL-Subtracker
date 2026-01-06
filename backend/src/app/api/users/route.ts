c1b6932cf68524bb84e97113c22ddb9e1aa6db0c
zaalie <zaaalie24@gmail.com>
2026-01-06 16:40:54 +0700

    reiza : memperbaiki code pada file route.ts pada users

--- backend/src/app/api/users/route.ts
+++ backend/src/app/api/users/route.ts
@@ -1,3 +1,15 @@
 import { prisma } from "@/lib/prisma";
-import { apiResponse } from "@/lib/response"; 
+import { apiResponse } from "@/lib/response";
+import bcrypt from "bcryptjs";
 
+export async function POST(req: Request) {
+  try {
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return apiResponse.badRequest("Data tidak lengkap");
+    }
+
+    const exists = await prisma.user.findUnique({
+      where: { email },
+    });
