17b21803288e648a5cd52e914cd3c0ee95887c13
zaalie <zaaalie24@gmail.com>
2026-01-06 16:52:48 +0700

    reiza : tambah code di file route.ts pada subscriptions

--- backend/src/app/api/subscriptions/route.ts
+++ backend/src/app/api/subscriptions/route.ts
@@ -1,3 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { apiResponse } from "@/lib/response";
 
+export async function POST(req: Request) {
+  try {
+    const data = await req.json();
+
+    const subscription = await prisma.subscription.create({
+      data,
+    });
+
+    return apiResponse.created(subscription);
+  } catch {
+    return apiResponse.serverError();
+  }
+}
