62772d937b513901ca5619b42bbe7611595f3c15
zaalie <zaaalie24@gmail.com>
2026-01-08 01:00:53 +0700

    reiza : mengedit dan tambah code program register di folder auth pada file route.ts

--- backend/src/app/api/auth/route.ts
+++ backend/src/app/api/auth/route.ts
@@ -2,16 +2,24 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-// AUTH (REGISTER & LOGIN)
+// AUTH (REGISTER & LOGIN) //
 export async function POST(req: Request) {
   try {
     const { action, name, email, password } = await req.json()
 
-// VALIDASI UMUM
+// VALIDASI UMUM //
     if (!action || !email || !password) {
       return NextResponse.json(
         { success: false, message: "Data wajib belum lengkap" },
         { status: 400 }
       )
     }
-    
+
+// REGISTER //
+if (action === "register") {
+      if (!name) {
+        return NextResponse.json(
+          { success: false, message: "Nama wajib diisi" },
+          { status: 400 }
+        )
+      }
