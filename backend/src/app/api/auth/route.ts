997412860daa45a8a829893105ce92f44eb7a378
zaalie <zaaalie24@gmail.com>
2026-01-08 00:56:24 +0700

    reiza : mengedit dan tambah code program regist & login di folder auth pada file route.ts

--- backend/src/app/api/auth/route.ts
+++ backend/src/app/api/auth/route.ts
@@ -2,3 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
+// AUTH (REGISTER & LOGIN)
+export async function POST(req: Request) {
+  try {
+    const { action, name, email, password } = await req.json()
