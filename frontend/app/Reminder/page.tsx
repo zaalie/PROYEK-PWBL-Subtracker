ba00bdc4203f6db2ecd6ecdae99cc3ebe5a43a5d
anjull2121 <annisa_zulfa_ulya@teknokrat.ac.id>
2026-01-17 22:00:58 +0700

    annisa-Menambahkan handleChange untuk form reminder

--- frontend/app/Reminder/page.tsx
+++ frontend/app/Reminder/page.tsx
@@ -8,3 +8,10 @@ export default function ReminderPage() {
     tanggal: "",
     waktu: "1", // H-1 default
   });
+
+  
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setReminder({ ...reminder, [e.target.name]: e.target.value });
+  };
