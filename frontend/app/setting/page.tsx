8d39a48f4f10d4f33fd39a5e773250fc29d233e8
anjull2121 <annisa_zulfa_ulya@teknokrat.ac.id>
2026-01-17 21:36:56 +0700

    annisa-Menambahkan styling subtitle

--- frontend/app/setting/page.tsx
+++ frontend/app/setting/page.tsx
@@ -112,3 +112,10 @@ export default function SettingPage() {
           text-align: center;
           margin-bottom: 6px;
         }
+
+        .subtitle {
+          text-align: center;
+          font-size: 14px;
+          color: #cbd5f5;
+          margin-bottom: 25px;
+        }
